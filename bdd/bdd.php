c1caa115d28dce7a90d0a4f8c04702cf23fee6d9
Allan <allan.miret@gmail.com>
2021-10-18 21:35:41 +0200

    finalisation

--- bdd/bdd.php
+++ bdd/bdd.php
@@ -8,34 +8,30 @@ Class Bdd {
     function __construct(){
 
         try {
-            $this->bdd = new PDO('mysql:host=127.0.0.1;port=3308;dbname=php_cours;charset=utf8','root','');
+            $this->bdd = new PDO('mysql:host=127.0.0.1;port=3308;dbname=exo_random_java;charset=utf8','root','');
 
         } catch (PDOException $e){
             echo $e->getMessage();
         }
     }
 
-    function getAllArticles() {
-        $sql = "SELECT id, titre, contenu, date_add, idusers FROM article INNER JOIN users ON fk_user = idusers";
-        $sth = $this->bdd->prepare($sql);
-        $sth->execute();
+    }
+
+    //N'est pas protégée
+    function affichageUtilisateurNonProtege($username) {
+
+        $sql = "SELECT u.username FROM users u WHERE username = :username;";
+        $sth = $sql;
+        $sth->execute(array(':username' => $username));
         return $sth -> fetchAll();
 
     }
 
-    function getUnArticle($id) {
-        $sql = "SELECT titre, contenu, date_add, idusers FROM article INNER JOIN users ON fk_user = idusers WHERE id = :id";
-        $sth = $this->bdd->prepare($sql);
-        $sth->execute(array(':id' => $id));
-        return $sth -> fetchAll();
-    }
-
-    function checkConnexion($username, $password){
+    function affichageUtilisateurProtege($username){
         $sql = "SELECT u.username
                 FROM users u
-                WHERE u.username = :username AND u.password = :pwd";
+                WHERE u.username = :username";
         $sth = $this -> bdd->prepare($sql);
-        $sth->execute(array(':username'=> $username, ':pwd' => $password));
+        $sth->execute(array(':username'=> $username));
             return $sth -> fetch();
     }
-}
--- tests/BddTest.php
+++ tests/BddTest.php
@@ -5,20 +5,62 @@
     // Pour créer une fonction de test il faut qu'elle hérite de la classe TestCase.
     // le nom de la fonction doit débuter par test
 
-    public function testAucunLoginEtMpd_connexion_DevraitRenvoyer0() {
+    public function testAucunUsername_affichageUtilisateurs_DevraitRenvoyer0() {
 
         // Arrange
-
-        $nom = "";
-        $mdp = "";
+        $username = "";
         $bdd = new Bdd();
 
         // Act
-        $utilisateur[] = $bdd->affichage($nom, $mdp);
+        $utilisateur[] = $bdd->affichageUtilisateurs($username);
         $obtenu = count($utilisateur);
-        $attendu = 0;
 
         // Assert
-        assertEquals($attendu, $obtenu, "Aucun utilisateur ne doit être retourner");
+        assertEquals(0, $obtenu, "Aucun utilisateur ne doit être retourné");
     }
+
+    public function testUsernameMultipleUser1_affichageUtilisateursNonProtege_DevraitRenvoyer2() {
+
+        // Arrange
+        $username = "user1";
+        $bdd = new Bdd();
+
+        // Act
+        $utilisateur[] = $bdd->affichageUtilisateurs($username);
+        $obtenu = count($utilisateur);
+
+        // Assert
+        assertEquals(2, $obtenu, "Deux utilisateurs doivent être retournés");
+    }
+
+    public function testUsernameUniqueEstAllan_affichageUtilisateursNonProtege_DevraitRenvoyer1() {
+
+        // Arrange
+
+        $username = "Allan";
+        $bdd = new Bdd();
+
+        // Act
+        $utilisateur[] = $bdd->affichageUtilisateursNonProtege($username);
+        $obtenu = count($utilisateur);
+
+        // Assert
+        assertEquals(1, $obtenu, "Un client doit être retourné");
+    }
+
+    public function testInjectionSQL_affichageUtilisateursNonProtege_DevraitRenvoyerCinqClients() {
+
+        // Arrange
+
+        $username = "Allan OR '1' = '1'";
+        $bdd = new Bdd();
+
+        // Act
+        $utilisateur[] = $bdd->affichageUtilisateursNonProtege($username);
+        $obtenu = count($utilisateur);
+
+        // Assert
+        assertEquals(5, $obtenu, "5 clients doivent être retournés");
+    }
+
     }
